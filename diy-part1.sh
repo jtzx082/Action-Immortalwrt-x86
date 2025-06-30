5d824ddb30e044b1eab17c6736f0e8f36aceebec
jtzx082 <yujq804@gmail.com>
2025-06-30 22:14:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,3 +26,4 @@ echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplan.git' >>feed
 echo 'src-git timecontrol https://github.com/sirpdboy/luci-app-timecontrol.git' >>feeds.conf.default
 echo 'src-git kucat https://github.com/sirpdboy/luci-theme-kucat.git' >>feeds.conf.default
 echo 'src-git kucat-config https://github.com/sirpdboy/luci-app-kucat-config.git' >>feeds.conf.default
+echo 'src-git parentcontrol https://github.com/sirpdboy/luci-app-parentcontrol.git' >>feeds.conf.default
