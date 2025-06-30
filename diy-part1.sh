b847d2c188b24964fefa068cb3d27fd743bbdf12
jtzx082 <yujq804@gmail.com>
2025-06-30 23:25:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@ echo 'src-git netwizard https://github.com/sirpdboy/luci-app-netwizard.git' >>fe
 echo 'src-git watchdog https://github.com/sirpdboy/luci-app-watchdog.git' >>feeds.conf.default
 echo 'src-git advancedplus https://github.com/sirpdboy/luci-app-advancedplus.git' >>feeds.conf.default
 echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplan.git' >>feeds.conf.default
-echo 'src-git timecontrol https://github.com/sirpdboy/luci-app-timecontrol.git' >>feeds.conf.default
+echo 'src-git timecontrol https://github.com/sirpdboy/luci-app-timecontrol.git;main' >>feeds.conf.default
 echo 'src-git kucat https://github.com/sirpdboy/luci-theme-kucat.git' >>feeds.conf.default
 echo 'src-git kucat-config https://github.com/sirpdboy/luci-app-kucat-config.git' >>feeds.conf.default
 echo 'src-git parentcontrol https://github.com/sirpdboy/luci-app-parentcontrol.git' >>feeds.conf.default
