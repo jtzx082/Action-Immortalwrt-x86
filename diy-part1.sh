a639055d046bb7c7f5240ba2cf757b9216e5f3a5
jtzx082 <yujq804@gmail.com>
2025-06-30 21:57:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,4 @@ echo 'src-git netspeedtest https://github.com/sirpdboy/luci-app-netspeedtest.git
 echo 'src-git netwizard https://github.com/sirpdboy/luci-app-netwizard.git' >>feeds.conf.default
 echo 'src-git watchdog https://github.com/sirpdboy/luci-app-watchdog.git' >>feeds.conf.default
 echo 'src-git advancedplus https://github.com/sirpdboy/luci-app-advancedplus.git' >>feeds.conf.default
+echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplan.git' >>feeds.conf.default
